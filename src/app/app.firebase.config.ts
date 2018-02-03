447ae9de5fedb1c63679957ed3575a5d0ffe3a90
Nick Salt <nicksalt24@gmail.com>
2018-02-03 18:35:55 -0500

    Update .gitignore

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG =  {
-    apiKey: "AIzaSyBc_zwdwZQWPlUDxXFY5Hm0GBrYFhyTHl0",
-    authDomain: "blackburn-1998.firebaseapp.com",
-    databaseURL: "https://blackburn-1998.firebaseio.com",
-    projectId: "blackburn-1998",
-    storageBucket: "blackburn-1998.appspot.com",
-    messagingSenderId: "366272627337"
-  };
